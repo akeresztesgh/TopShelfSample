8e9d2f977b59448969354fd84dc6fdd1b38d06d7
AJ Keresztes <aj@keresztes.us>
2022-02-22 13:54:16 -0500

    topshelf example

---
+++ TopShelfSample/MyServiceHandler.cs
@@ -0,0 +1,38 @@
+﻿using System.Timers;
+
+namespace TopShelfSample
+{
+    public class MyServiceHandler
+    {
+        private System.Timers.Timer aTimer;
+
+        public void Start()
+        {
+            // startup code here
+            // run thread, start timer, or connect to external service (like a queue)
+            SetTimer();
+        }
+
+        private void SetTimer()
+        {
+            // Create a timer with a two second interval.
+            aTimer = new System.Timers.Timer(2000);
+            // Hook up the Elapsed event for the timer. 
+            aTimer.Elapsed += OnTimedEvent;
+            aTimer.AutoReset = true;
+            aTimer.Enabled = true;
+        }
+
+        private void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            Console.WriteLine("The Elapsed event was raised at {0:HH:mm:ss.fff}",
+                              e.SignalTime);
+        }
+
+        public void Stop()
+        {
+            // cleanup
+            aTimer.Stop();
+        }
+    }
+}
---
+++ TopShelfSample/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Runtime.InteropServices;
+using Topshelf;
+using Topshelf.Runtime.DotNetCore;
+using TopShelfSample;
+
+Console.WriteLine("MyService Starting");
+
+
+// Main entry
+var rc = HostFactory.Run(x =>
+{
+    // this works if running on Linux
+    if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+    {
+        x.UseEnvironmentBuilder(new Topshelf.HostConfigurators.EnvironmentBuilderFactory(c => {
+            return new DotNetCoreEnvironmentBuilder(c);
+        }));
+    }
+
+    x.Service<MyServiceHandler>(s =>
+    {
+        // The system can use dependency injection, if it's setup
+        //s.ConstructUsing(name => ServiceProvider.GetService<MyService>());
+        s.ConstructUsing(name => new MyServiceHandler());
+        // The start function
+        s.WhenStarted(tc => tc.Start());
+        // The stop function
+        s.WhenStopped(tc => tc.Stop());
+    });
+
+    // Run as local system user
+    x.RunAsLocalSystem();
+    // Description that shows up in the services control manager
+    x.SetDescription("MyService TopShelf sample");
+    // Displayed in services control manager
+    x.SetDisplayName("My-Service");
+    // Registry name
+    x.SetServiceName("MyService");
+    // Configure for delayed auto-start 
+    x.StartAutomaticallyDelayed();
+});
+
+Console.WriteLine("MyService Halted");
+var exitCode = (int)Convert.ChangeType(rc, rc.GetTypeCode());
+Environment.ExitCode = exitCode;
+
